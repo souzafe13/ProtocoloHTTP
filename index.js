3199e5a8469bb56a17318f8c32ada15ce278b60b
souzafe13 <souzafe13@gmail.com>
2024-03-06 17:05:00 -0300

    Rotas

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const app = express();
 
 // rota 1
 app.get("/", function(req, res){
-    res.send("boas vindas!");
+    res.send("boas vindas ao meu app!");
 });
+
 // rota 2
 app.get("/sobre", function(req, res){
     res.send("sobre a minha empresa!");
 });
+
 // rota 3
 app.get("/produtos", function(req, res){
     res.send("Confira os nossos produtos");
 });
+
+//rota com parâmetros
+app.get("/ola/:nome/:cargo/:idade", function(req, res){
+res.send("<h1>Ola " + req.params.nome + "</h1>" + 
+"<br><h2> Seu cargo é: " + req.params.cargo + 
+"<br> Sua idade é: " + req.params.idade + "</h2>"
+);
+});
+
+
+
 //porta do protocolo http
 app.listen(8081, function(){
 console.log("servidor está rodando no endereço http://localhost:8081");   
